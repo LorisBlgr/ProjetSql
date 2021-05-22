fc0dced230e00bed2f36664b692799c0b786711e
LorisBlgr <78910790+LorisBlgr@users.noreply.github.com>
2021-05-22 14:41:06 +0200

    Delete requêteSql directory

--- requêteSql/requêtes/req13.sql
+++
@@ -1,6 +0,0 @@
-SELECT numVotes,primaryTitle,genres
-FROM title_ratings
-JOIN title_basics ON title_ratings.tconst = title_basics.tconst
-WHERE genres='Animation' AND numVotes>1000
-ORDER BY numVotes DESC 
-LIMIT 10;
